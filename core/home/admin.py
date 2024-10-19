6e30ebd3c40937909d212dd202f3f96297b48b3c
Rishikesh <rishikeshjagadale19@gmail.com>
2024-10-20 00:05:08 +0530

    Register basic models

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Receipe, Ingredients
+
+admin.site.register(Receipe)
+admin.site.register(Ingredients)
--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Receipe(models.Model):
+    receipe_name = models.CharField(max_length=100, db_index=True)
+    receipe_description = models.TextField()
+    receipe_image = models.ImageField(upload_to="recepie/")
+    receipe_slug = models.SlugField(unique=True)
+    receipe_type = models.CharField(max_length=100, choices=(("veg", "veg"), ("non-veg", "non-veg")))
+
+class Ingredients(models.Model):
+    receipe = models.ForeignKey(Receipe, on_delete=models.CASCADE, related_name="receipe_ingredients")
+    ingredients_name = models.CharField(max_length=100)
