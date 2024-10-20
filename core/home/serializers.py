683a1ce12d86cf32c0a8daf12ca4c67ace779078
Rishikesh <rishikeshjagadale19@gmail.com>
2024-10-20 13:25:00 +0530

    APIs

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -1,21 +1,21 @@
 from rest_framework import serializers
 from .models import Receipe, Ingredients
 
-
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Ingredients  # Replace with your Ingredient model
-        fields = '__all__'  # You can specify the fields or use '__all__'
+        model = Ingredients
+        fields = '__all__'
 
 class ReceipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, required=False)
+
     class Meta:
-        model = Receipe  # Your model here
-        fields = '__all__'  # or specify the fields you want, e.g., ['id', 'name', 'description']
+        model = Receipe
+        fields = '__all__'
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['ingredients'] = IngredientSerializer(
-            instance.ingredients.all(), many=True
-        ).data
-
-        return data
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients', [])
+        receipe = Receipe.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:
+            Ingredients.objects.create(receipe=receipe, **ingredient_data)
+        return receipe
--- core/home/views.py
+++ core/home/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ReceipeSerializer
-from .models import Receipe, Ingredients
+from .models import Receipe
 
-# Create your views here.
 class ReceipeAPI(APIView):
 
     def get(self, request):
         queryset = Receipe.objects.all()
-        serializer = ReceipeSerializer(queryset, many = True)
+        serializer = ReceipeSerializer(queryset, many=True)
         return Response({
             "status": True,
-            "message" : "data fetched",
+            "message": "data fetched",
             "data": serializer.data,
         })
+
+    def post(self, request):
+        serializer = ReceipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "status": True,
+                "message": "data is created",
+                "data": serializer.data,
+            }, status=201)
+        return Response({
+            "status": False,
+            "message": "data not created",
+            "data": serializer.errors,
+        }, status=400)
